6dac953ce28b1d93edd897f895fc1b438ff8f605
André Luiz de Amorim <streetomon@gmail.com>
2014-07-17 14:58:14 -0300

    Fixed bug where all pixels were masked when fitting without setting a mask.

--- imfit/fitting.py
+++ imfit/fitting.py
@@ -132,7 +132,7 @@ class Imfit(object):
                 mask = image.mask
             image = image.filled(fill_value=0.0)
         if mask is None:
-            mask = np.ones_like(image)
+            mask = np.zeros_like(image)
         else:
             self._mask = mask.astype('bool')
 
